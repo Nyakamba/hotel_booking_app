34c687926790922d5ad3937a472736a8f570976a
Nyakamba <omwegaenock@gmail.com>
2024-08-06 14:16:56 +0300

    added the header component

---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Layout = () => {
+  return (
+    <div className="felx flex-col min-h-screen">
+      <Header />
+    </div>
+  );
+};
+
+export default Layout;
