17ea4a00f0b6641d6783d74bf7271dae181dffe4
Nyakamba <omwegaenock@gmail.com>
2024-08-06 22:01:46 +0300

    added login api funtionality

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,41 +1,46 @@
 import express, { Request, Response } from "express";
-import jwt from "jsonwebtoken";
 import { check, validationResult } from "express-validator";
-
 import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.post(
-  "/register",
+  "/login",
   [
-    check("firstName", "First name is required").isString(),
-    check("lastName", "Last name is required").isString(),
     check("email", "Email is required").isEmail(),
-    check(
-      "password",
-      "Password with 6 or more characters is required"
-    ).isLength({ min: 6 }),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
   ],
   async (req: Request, res: Response) => {
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ message: errors.array() });
     }
-    try {
-      let user = await User.findOne({ email: req.body.email });
 
-      if (user) {
-        return res.status(400).json({ messsage: "User already exists" });
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Credentials" });
       }
 
-      user = new User(req.body);
-      await user.save();
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Credentials" });
+      }
 
       const token = jwt.sign(
         { userId: user.id },
         process.env.JWT_SECRET_KEY as string,
-        { expiresIn: "1d" }
+        {
+          expiresIn: "1d",
+        }
       );
 
       res.cookie("auth_token", token, {
@@ -43,10 +48,10 @@ router.post(
         secure: process.env.NODE_ENV === "production",
         maxAge: 86400000,
       });
-      return res.sendStatus(200);
+      res.status(200).json({ userId: user._id });
     } catch (error) {
       console.log(error);
-      res.status(500).send({ message: "Something went wrong" });
+      res.status(500).json({ message: "Something went wrong" });
     }
   }
 );
