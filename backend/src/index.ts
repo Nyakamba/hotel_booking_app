7c5b75ad33f3b92837481f422bcfc86f7d4f7a99
Nyakamba <omwegaenock@gmail.com>
2024-08-06 16:15:52 +0300

    completed user register functionality

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -9,9 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json("hello from express endpoint!");
-});
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
   console.log("Server running on localhost:7000");
