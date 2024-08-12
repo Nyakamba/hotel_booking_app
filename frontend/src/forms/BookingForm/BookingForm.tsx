916cc2d90b7c1b525a0a347bb7648baf0c4c9586
Nyakamba <omwegaenock@gmail.com>
2024-08-12 13:42:39 +0300

    booking confirmation page

---
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -0,0 +1,83 @@
+import { useForm } from "react-hook-form";
+import { UserType } from "../../../../backend/src/shared/types";
+import { useParams } from "react-router-dom";
+import { useSearchContext } from "../../contexts/SearchContext";
+import { useAppContext } from "../../contexts/AppContext";
+
+type Props = {
+  currentUser: UserType;
+};
+
+export type BookingFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  adultCount: number;
+  childCount: number;
+  checkIn: string;
+  checkOut: string;
+  hotelId: string;
+  paymentIntentId: string;
+  totalCost: number;
+};
+
+const BookingForm = ({ currentUser }: Props) => {
+  const search = useSearchContext();
+  const { hotelId } = useParams();
+
+  const { showToast } = useAppContext();
+
+  const { handleSubmit, register } = useForm<BookingFormData>({
+    defaultValues: {
+      firstName: currentUser.firstName,
+      lastName: currentUser.lastName,
+      email: currentUser.email,
+      adultCount: search.adultCount,
+      childCount: search.childCount,
+      checkIn: search.checkIn.toISOString(),
+      checkOut: search.checkOut.toISOString(),
+      hotelId: hotelId,
+      //   totalCost: paymentIntent.totalCost,
+      //   paymentIntentId: paymentIntent.paymentIntentId,
+    },
+  });
+  return (
+    <form className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-5">
+      <span className="text-3xl font-bold">Confirm Your Details</span>
+      <div className="grid grid-cols-2 gap-6">
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          First Name
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
+            type="text"
+            readOnly
+            disabled
+            {...register("firstName")}
+          />
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Last Name
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
+            type="text"
+            readOnly
+            disabled
+            {...register("lastName")}
+          />
+        </label>
+        <label className="text-gray-700 text-sm font-bold flex-1">
+          Email
+          <input
+            className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
+            type="text"
+            readOnly
+            disabled
+            {...register("email")}
+          />
+        </label>
+      </div>
+    </form>
+  );
+};
+
+export default BookingForm;
