a4b9274774f1dc88b8ec181106db3db26af6c854
Nyakamba <omwegaenock@gmail.com>
2024-08-12 21:00:39 +0300

    stripe payment

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -5,8 +5,11 @@ import BookingDetailsSummary from "../components/BookingDetailsSummary";
 import { useParams } from "react-router-dom";
 import { useSearchContext } from "../contexts/SearchContext";
 import { useEffect, useState } from "react";
+import { useAppContext } from "../contexts/AppContext";
+import { Elements } from "@stripe/react-stripe-js";
 
 const Booking = () => {
+  const { stripePromise } = useAppContext();
   const search = useSearchContext();
   const { hotelId } = useParams();
 
@@ -21,6 +24,18 @@ const Booking = () => {
       setNumberOfNights(Math.ceil(nights));
     }
   }, [search.checkIn, search.checkOut]);
+
+  const { data: paymentIntentData } = useQuery(
+    "createPaymentIntent",
+    () =>
+      apiClient.createPaymentIntent(
+        hotelId as string,
+        numberOfNights.toString()
+      ),
+    {
+      enabled: !!hotelId && numberOfNights > 0,
+    }
+  );
 
   const { data: hotel } = useQuery(
     "fetchHotelByID",
@@ -48,7 +63,19 @@ const Booking = () => {
         numberOfNights={numberOfNights}
         hotel={hotel}
       />
-      {currentUser && <BookingForm currentUser={currentUser} />}
+      {currentUser && paymentIntentData && (
+        <Elements
+          stripe={stripePromise}
+          options={{
+            clientSecret: paymentIntentData.clientSecret,
+          }}
+        >
+          <BookingForm
+            currentUser={currentUser}
+            paymentIntent={paymentIntentData}
+          />
+        </Elements>
+      )}
     </div>
   );
 };
